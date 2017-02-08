0a1311659eb899385de51fe712f89e369d3c058c
DESKTOP-99N58E4\Razva <razvan.comsa@digital-partner.ro>
2017-02-08 16:34:37 +0200

    hoc overview

---
+++ src/components/resources.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (<div> recipe ..
+      <ul>
+          <li>sugar</li>
+          <li>spice</li>
+          <li>everything nice</li>
+      </ul>
+  </div>);
+};
